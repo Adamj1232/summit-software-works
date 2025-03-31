a1bd7d1081b4eb32eb78eaddf012bc412a871f11
Adam Carpenter <adamcarpenter86@gmail.com>
2025-03-31 17:15:28 -0600

    Fix mobile menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -168,7 +168,7 @@ const Navbar = () => {
                   border-l border-white/10 z-50 md:hidden"
               >
                 <div className="flex flex-col h-full">
-                  <div className="flex justify-between items-center p-4 border-b border-white/10">
+                  <div className="flex justify-between items-center pt-4 px-4 border-b border-white/10">
                     <Logo size="small" />
                     <button
                       onClick={closeMobileMenu}
@@ -187,7 +187,7 @@ const Navbar = () => {
                       </svg>
                     </button>
                   </div>
-                  <nav className="flex-1 px-4 py-6 space-y-2">
+                  <nav className="flex-1 px-4 py-6 space-y-2 bg-mountain-900">
                     {navItems.map((item) => (
                       <MobileNavLink
                         key={item.path}
